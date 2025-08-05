3bbb9e40f8a5841d412ffc9f112fc0008fd9a86a
Shape581 <severusrogue5008@gmail.com>
2025-08-05 13:12:55 +0200

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mileage581
+{
+    public class Config
+    {
+        public float multiplier = 1.0f;
+    }
+}
---
+++ Mileage.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mileage581
+{
+    public class Mileage
+    {
+        [AutoIncrement][PrimaryKey] public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public float Value { get; set; }
+    }
+}
